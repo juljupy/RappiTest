a9975d0d40b2cca529a06fd0a6f7ed303cd72199
Julio De Hoyos <juljupy@gmail.com>
2016-09-03 10:28:07 -0500

    Front end Response
    Parsing backend response for being showed in front end.

--- public/js/matrix3d.js
+++ public/js/matrix3d.js
@@ -1,15 +1,16 @@
 $(document).ready(function(){
 	$( "#cube-form" ).submit(function( event ) {
-		/*HoldOn.open({
+		HoldOn.open({
             message:'Evaluando...'
-        });*/
+        });
 		$.ajax({
 			type: "POST",
 			url: '/evaluatematrix',
 			data: $( "#cube-form" ).serialize(),
 			success: function(res){
-				$( "#result-panel" ).html(res.data.cases);
-				//HoldOn.close();
+				var resp = res.data.results.join('<br>');
+				$( "#result-panel" ).html(resp);
+				HoldOn.close();
 			},
 			dataType: 'json'
 		});
